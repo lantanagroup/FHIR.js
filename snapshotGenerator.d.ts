1b44c7904285e6cfe0bb4e033c73566d3685bda7
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-04-24 15:52:26 -0700

    Updates for documentation

--- snapshotGenerator.d.ts
+++ snapshotGenerator.d.ts
@@ -1,13 +1,12 @@
 import { Bundle, ParseConformance, StructureDefinition } from './parseConformance';
 export declare class SnapshotGenerator {
-    readonly baseUrl = "http://hl7.org/fhir/StructureDefinition/";
-    readonly choiceRegexString = "(Instant|Time|Date|DateTime|Decimal|Boolean|Integer|String|Uri|Base64Binary|Code|Id|Oid|UnsignedInt|PositiveInt|Markdown|Url|Canonical|Uuid|Identifier|HumanName|Address|ContactPoint|Timing|Quantity|SimpleQuantity|Attachment|Range|Period|Ratio|CodeableConcept|Coding|SampledData|Age|Distance|Duration|Count|Money|MoneyQuantity|Annotation|Signature|ContactDetail|Contributor|DataRequirement|ParameterDefinition|RelatedArtifact|TriggerDefinition|UsageContext|Expression|Reference|Narrative|Extension|Meta|ElementDefinition|Dosage|Xhtml)";
-    readonly parser: ParseConformance;
-    readonly bundle: Bundle;
+    private readonly choiceRegexString;
+    private readonly parser;
+    private readonly bundle;
     private processedUrls;
     constructor(parser: ParseConformance, bundle: Bundle);
     static createBundle(...structureDefinitions: StructureDefinition[]): Bundle;
-    private getBaseStructureDefinition;
+    private getStructureDefinition;
     private process;
     generate(): void;
 }
--- snapshotGenerator.ts
+++ snapshotGenerator.ts
@@ -1,19 +1,45 @@
 import {Bundle, ElementDefinition, ParseConformance, ParsedProperty, StructureDefinition} from './parseConformance';
 import * as _ from 'underscore';
 
+/**
+ * Responsible for creating snapshots on StructureDefinition resources based on the differential of the profile.
+ */
 export class SnapshotGenerator {
-    readonly baseUrl = 'http://hl7.org/fhir/StructureDefinition/';
-    readonly choiceRegexString = '(Instant|Time|Date|DateTime|Decimal|Boolean|Integer|String|Uri|Base64Binary|Code|Id|Oid|UnsignedInt|PositiveInt|Markdown|Url|Canonical|Uuid|Identifier|HumanName|Address|ContactPoint|Timing|Quantity|SimpleQuantity|Attachment|Range|Period|Ratio|CodeableConcept|Coding|SampledData|Age|Distance|Duration|Count|Money|MoneyQuantity|Annotation|Signature|ContactDetail|Contributor|DataRequirement|ParameterDefinition|RelatedArtifact|TriggerDefinition|UsageContext|Expression|Reference|Narrative|Extension|Meta|ElementDefinition|Dosage|Xhtml)';
+    /**
+     * A string that represents all options possible in STU3 and R4 for choice elements, ex: value[x], effective[x], etc.)
+     * It is used to create a regex that tests that value[x] matches valueQuantity in an ElementDefinition.path.
+     */
+    private readonly choiceRegexString = '(Instant|Time|Date|DateTime|Decimal|Boolean|Integer|String|Uri|Base64Binary|Code|Id|Oid|UnsignedInt|PositiveInt|Markdown|Url|Canonical|Uuid|Identifier|HumanName|Address|ContactPoint|Timing|Quantity|SimpleQuantity|Attachment|Range|Period|Ratio|CodeableConcept|Coding|SampledData|Age|Distance|Duration|Count|Money|MoneyQuantity|Annotation|Signature|ContactDetail|Contributor|DataRequirement|ParameterDefinition|RelatedArtifact|TriggerDefinition|UsageContext|Expression|Reference|Narrative|Extension|Meta|ElementDefinition|Dosage|Xhtml)';
 
-    readonly parser: ParseConformance;
-    readonly bundle: Bundle;
+    /**
+     * The parser containing FHIR versioning and base profile information
+     */
+    private readonly parser: ParseConformance;
+
+    /**
+     * The bundle passed into the constructor whose structure definitions should have snapshots generated
+     */
+    private readonly bundle: Bundle;
+
+    /**
+     * A field that tracks what profiles have been processed during the .generate() operation, so that profiles are not processed multiple times in a single run.
+     */
     private processedUrls: string[] = [];
 
+    /**
+     *
+     * @param parser (ParseConformance) The parser that holds FHIR version information, and has base FHIR structures loaded in it
+     * @param bundle The bundle including structure definitions whose snapshot should be generated
+     */
     constructor(parser: ParseConformance, bundle: Bundle) {
         this.parser = parser;
         this.bundle = bundle;
     }
 
+    /**
+     * Creates a bundle out of StructureDefinition resources. This is just for making it easier to pass a bundle to the constructor of this class.
+     * @param structureDefinitions
+     */
     static createBundle(...structureDefinitions: StructureDefinition[]) {
         const bundle: Bundle = {
             resourceType: 'Bundle',
@@ -25,10 +51,16 @@ export class SnapshotGenerator {
         return bundle;
     }
 
-    private getBaseStructureDefinition(url: string, type: string) {
+    /**
+     * Gets a StructureDefinition based on the url and type. First determines if the url represents a base resource in the FHIR spec, then looks through
+     * the bundle passed to the constructor of the class to find the structure.
+     * @param url The url of the profile to retrieve
+     * @param type The type of resource the profile constrains (ex: Composition, or Patient, or Person, etc.)
+     */
+    private getStructureDefinition(url: string, type: string) {
         const isBaseProfile = this.parser.isBaseProfile(url);
         const fhirBase = isBaseProfile ?
-            _.find(this.parser.structureDefinitions, (sd) => sd.url.toLowerCase() === (this.baseUrl + type).toLowerCase()) :
+            _.find(this.parser.structureDefinitions, (sd) => sd.url.toLowerCase() === ('http://hl7.org/fhir/StructureDefinition/' + type).toLowerCase()) :
             null;
 
         if (isBaseProfile && !fhirBase) {
@@ -49,6 +81,11 @@ export class SnapshotGenerator {
         return parentEntry.resource;
     }
 
+    /**
+     * Generates a snapshot for the specified structure definition. If the structure definition is based on another custom profile, that
+     * custom profile is found in the bundle passed to the constructor, and the snapshot is generated for the base profile, as well (recursively).
+     * @param structureDefinition The profile to create a snapshot for
+     */
     private process(structureDefinition: StructureDefinition) {
         if (this.parser.isBaseProfile(structureDefinition.url) || this.processedUrls.indexOf(structureDefinition.url) >= 0) {
             return;
@@ -58,7 +95,7 @@ export class SnapshotGenerator {
             throw new Error(`Structure ${structureDefinition.url} does not have a differential.`);
         }
 
-        const base = this.getBaseStructureDefinition(structureDefinition.baseDefinition, structureDefinition.type);
+        const base = this.getStructureDefinition(structureDefinition.baseDefinition, structureDefinition.type);
         const newElements: ElementDefinition[] = JSON.parse(JSON.stringify(base.snapshot.element));
         const matched = _.filter(newElements, (newElement) => {
             if (newElement.path === structureDefinition.type) {
@@ -106,6 +143,10 @@ export class SnapshotGenerator {
         this.processedUrls.push(structureDefinition.url);
     }
 
+    /**
+     * Generates a snapshot for all structure definitions in the bundle. If a structure definition in the bundle is a base FHIR structure definition, it is
+     * assumed the structure definition already has a snapshot, and it is ignored.
+     */
     generate() {
         this.processedUrls = [];
 
